d5277fdef43005993dd34018ac8e412e5ebfa707
Sana Phanendra <phanendrasanagfx7781@gmail.com>
2026-03-26 18:26:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-function goToBooking() {
+function goToBooking(){
   document.getElementById("booking").scrollIntoView();
 }
 
-function openLogin() {
-  alert("Login feature coming soon");
+function openLogin(){
+  document.getElementById("loginModal").style.display="block";
 }
 
-function bookNow() {
-  let phone = document.getElementById("phone").value;
-  let date = document.getElementById("date").value;
+function openCreator(){
+  document.getElementById("creatorModal").style.display="block";
+}
 
-  if (!phone || !date) {
-    alert("Enter details");
+function login(){
+  let user = document.getElementById("loginUser").value;
+  localStorage.setItem("user", user);
+  alert("Logged in");
+  document.getElementById("loginModal").style.display="none";
+}
+
+function bookNow(){
+  let user = localStorage.getItem("user");
+
+  if(!user){
+    alert("Please login first");
     return;
   }
 
+  let plan = document.getElementById("plan").value;
+  let phone = document.getElementById("phone").value;
+  let date = document.getElementById("date").value;
+
   let bookings = JSON.parse(localStorage.getItem("bookings")) || [];
 
-  bookings.push({ phone, date });
+  bookings.push({user,plan,phone,date});
 
   localStorage.setItem("bookings", JSON.stringify(bookings));
 
-  document.getElementById("message").innerText =
-    "Booking Confirmed ✅";
+  document.getElementById("msg").innerText="Booking Confirmed 🎬";
 }
