b9bed8f67d0448b69e0551cc399ce49aedc0b0e3
Sana Phanendra <phanendrasanagfx7781@gmail.com>
2026-03-26 18:35:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ function openLogin(){
   document.getElementById("loginModal").style.display="block";
 }
 
-function openCreator(){
-  document.getElementById("creatorModal").style.display="block";
-}
-
 function login(){
   let user = document.getElementById("loginUser").value;
+
+  if(!user){
+    alert("Enter details");
+    return;
+  }
+
   localStorage.setItem("user", user);
   alert("Logged in");
   document.getElementById("loginModal").style.display="none";
@@ -21,7 +23,7 @@ function bookNow(){
   let user = localStorage.getItem("user");
 
   if(!user){
-    alert("Please login first");
+    alert("Login first");
     return;
   }
 
@@ -29,11 +31,17 @@ function bookNow(){
   let phone = document.getElementById("phone").value;
   let date = document.getElementById("date").value;
 
+  if(!phone || !date){
+    alert("Fill details");
+    return;
+  }
+
   let bookings = JSON.parse(localStorage.getItem("bookings")) || [];
 
   bookings.push({user,plan,phone,date});
 
   localStorage.setItem("bookings", JSON.stringify(bookings));
 
-  document.getElementById("msg").innerText="Booking Confirmed 🎬";
+  document.getElementById("msg").innerText =
+    "Booking Confirmed 🎬";
 }
